2c6f9b6763b7f12ec57f3e026741e280e200e295
Samuel Adebayo <sambayo28@gmail.com>
2022-01-07 22:46:44 +0100

    added disabled functionality

--- libs/ui/tabs/src/index.ts
+++ libs/ui/tabs/src/index.ts
@@ -1,1 +1,4 @@
 export * from './lib/ui-tabs.module';
+export * from './lib/tab/tab.component';
+export * from './lib/tab-panel/tab-panel.component';
+export * from './lib/tablist/tablist.component';
