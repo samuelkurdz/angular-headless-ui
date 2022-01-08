db8d7913f19040aa4d9892e71fd81bffceb4ea1e
Samuel Adebayo <sambayo28@gmail.com>
2022-01-08 17:10:10 +0100

    improve a little accessibilties for tab lib

--- libs/ui/radio/src/lib/group/group.component.ts
+++ libs/ui/radio/src/lib/group/group.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'headless-radio-group',
   templateUrl: './group.component.html',
 })
 export class RadioGroupComponent {
-
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  @Input() defaultOption: any;
 
 }
