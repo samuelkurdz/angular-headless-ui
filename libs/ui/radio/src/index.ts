91a29c0883b23e9af59e2e30aaf501ae8f69420c
Samuel Adebayo <sambayo28@gmail.com>
2022-01-11 08:33:09 +0100

    radio service removed, directive used instead, awhole rewrote

--- libs/ui/radio/src/index.ts
+++ libs/ui/radio/src/index.ts
@@ -1,5 +1,5 @@
 export * from './lib/ui-radio.module';
 export * from './lib/group/group.component';
 export * from './lib/label/label.component';
-export * from './lib/option/option.component';
+export * from './lib/radio-option.directive';
 
--- libs/ui/radio/src/lib/group/group.component.ts
+++ libs/ui/radio/src/lib/group/group.component.ts
@@ -1,38 +1,39 @@
-import { Component, EventEmitter, HostBinding, Input, OnDestroy, OnInit, Output } from '@angular/core';
-import { Subject, takeUntil } from 'rxjs';
-import { RadioService } from '../radio.service';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { RadioOptionDirective } from '../radio-option.directive';
 
 @Component({
   selector: 'headless-radio-group',
   templateUrl: './group.component.html',
 })
-export class RadioGroupComponent implements OnInit, OnDestroy {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  @Input() defaultOption: any;
-  @Input() Label!: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  @Output() currentRadioGroupValue = new EventEmitter<any>();
+export class RadioGroupComponent {
+  // @Input() defaultOption: any;
+  // @Input() Label!: string;
+  // @Output() currentRadioGroupValue = new EventEmitter<any>();
 
-  destroy$: Subject<boolean> = new Subject<boolean>();
+  // destroy$: Subject<boolean> = new Subject<boolean>();
 
-  @HostBinding('attr.aria-labelledby') @Input() radioGroupName!: string;
-  @HostBinding('attr.role') get role() { return 'radiogroup' }
+  // @HostBinding('attr.aria-labelledby') @Input() radioGroupName!: string;
+  // @HostBinding('attr.role') get role() { return 'radiogroup' }
+
+  tabs: RadioOptionDirective[] = [];
 
   constructor(
-    private radioService: RadioService
+    private renderer: Renderer2,
+    private elementRef: ElementRef
   ) { }
 
-  ngOnInit(): void {
-    this.radioService.changeRadioValue(this.defaultOption);
-    this.radioService.currentRadioValue
-    .pipe(takeUntil(this.destroy$))
-    .subscribe(index => {
-      this.currentRadioGroupValue.emit(index);
-    });
+  addTab(tab: RadioOptionDirective): void {
+    this.tabs.push(tab);
+    // tab.active = this.tabs.length === 1 && !tab.active;
   }
 
-  ngOnDestroy() {
-    this.destroy$.next(true);
-    this.destroy$.unsubscribe();
+  keyNavActions(event: KeyboardEvent, index: number) {
+    // event.preventDefault();
+    // const list: HTMLElement[] = Array.from(this.elementRef.nativeElement.querySelectorAll('.headless-radio'));
+    // console.log(list.length);
+    this.tabs.forEach((tab, tabIndex) => {
+      tabIndex === index ? tab.checked = true : tab.checked = false
+    })
   }
+
 }
