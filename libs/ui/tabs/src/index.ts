8b1618e2c548944517eb83b83b2274e4cf3d0fb1
Samuel Adebayo <sambayo28@gmail.com>
2022-01-14 10:14:18 +0100

    update workflow for on push to master, fix build error

--- libs/ui/tabs/src/index.ts
+++ libs/ui/tabs/src/index.ts
@@ -1,4 +1,4 @@
 export * from './lib/ui-tabs.module';
 export * from './lib/tab/tab.component';
-export * from './lib/tab-panel/tab-panel.component';
 export * from './lib/tab-group/tab-group.component';
+export * from './lib/tab-panel.directive';
--- libs/ui/tabs/src/lib/ui-tabs.module.ts
+++ libs/ui/tabs/src/lib/ui-tabs.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabComponent } from './tab/tab.component';
 import { TabGroupComponent } from './tab-group/tab-group.component';
-import { TabPanelComponent } from './tab-panel/tab-panel.component';
 import { TabPanelDirective } from './tab-panel.directive';
 
 @NgModule({
@@ -10,14 +9,12 @@ import { TabPanelDirective } from './tab-panel.directive';
   declarations: [
     TabComponent,
     TabGroupComponent,
-    TabPanelComponent,
     TabPanelDirective
   ],
   exports: [
     TabComponent,
     TabGroupComponent,
-    TabPanelComponent,
     TabPanelDirective
   ]
 })
-export class UiTabsModule {}
+export class UiTabsModule { }
