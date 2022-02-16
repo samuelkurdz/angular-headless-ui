615702e7eced3119f14e89e9440333dfb346edaa
Samuel Adebayo <sambayo28@gmail.com>
2022-02-16 19:58:26 +0100

    functional disclosure component

--- libs/ui/disclosure/README.md
+++ libs/ui/disclosure/README.md
@@ -1,7 +1,28 @@
 # ui-disclosure
 
-This library was generated with [Nx](https://nx.dev).
+Easily create accessible, fully customizable tab interfaces, with robust focus management and coming keyboard navigation support.
 
-## Running unit tests
+## Installation
 
-Run `nx test ui-disclosure` to execute the unit tests.
+```bash
+# npm
+npm install @ngheadlessui/disclosure
+
+# Yarn
+yarn add @ngheadlessui/disclosure
+```
+
+## Basic Example
+
+Tabs are built using the headless-tab-group, headless-tab, and headlessTabPanel components. By default the first tab is selected, and clicking on any tab will activate the corresponding panel.
+
+```html
+  <headless-disclosure>
+    <headless-disclosure-button>
+      buttoneText
+    </headless-disclosure-button>
+    <div headlessDisclosurePanel>
+      description
+    </div>
+  </headless-disclosure>
+```
--- libs/ui/disclosure/src/lib/disclosure/disclosure.component.ts
+++ libs/ui/disclosure/src/lib/disclosure/disclosure.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, HostBinding, Input, Output } from '@angular/core';
 import { DisclosureButtonComponent } from '../disclosure-button/disclosure-button.component';
 import { DisclosurePanelDirective } from '../disclosure-panel.directive';
 
@@ -11,21 +11,39 @@ export class DisclosureComponent {
   @Input() disabled!: boolean;
   @Output() panelChange: EventEmitter<boolean> = new EventEmitter();
 
+  @HostBinding('class.close-disclosure') get isShown(): boolean {
+    return this._isShown;
+  }
+
+  set isShown(hidden: boolean) {
+    if (this._isShown === hidden) {
+      return;
+    }
+
+    this._isShown = hidden;
+  }  
   
+  _isShown!: boolean;
   displayPanel!: DisclosurePanelDirective;
   disclosureButton!: DisclosureButtonComponent;
+
+  updateOpenState(state: boolean) {
+    this.panelChange.emit(state);
+    this.isShown = state;
+  }
 
   addPanel(panel: DisclosurePanelDirective): void {
     this.displayPanel = panel;
     this.panelChange.emit(panel.hidden);
+    this.updateOpenState(panel.hidden);
   }
-
+  
   addButton(button: DisclosureButtonComponent): void {
     this.disclosureButton = button;
   }
-
+  
   keyNavActions() {
     this.displayPanel.hidden = !this.displayPanel.hidden;
-    this.panelChange.emit(this.displayPanel.hidden);
+    this.updateOpenState(this.displayPanel.hidden);
   }
 }
